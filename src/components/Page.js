e25db4806d7801cfb90e788ba6fd09099b1a1a49
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-03-06 13:27:26 +0300

    'Making actions' completed

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Page extends Component {
+    onYearBtnClick = (e) => {
+        this.props.setYear(+e.target.innerText);
+    }
     render() {
-        const { year, photos } = this.props
+        const { year, photos } = this.props;
         return (
             <div>
-                <p>You have {photos.length} photos by {year} year</p>
+                <p>
+                    <button onClick={this.onYearBtnClick}>2272</button>
+                    <button onClick={this.onYearBtnClick}>2273</button>
+                    <button onClick={this.onYearBtnClick}>2274</button>
+                </p>
+                <h3>Year {year}</h3>
+                <p>You have {photos.length} photos by the year {year}.</p>
             </div>
         )
     }
@@ -16,5 +25,6 @@ export default class Page extends Component {
 
 Page.propTypes = {
     year: PropTypes.number.isRequired,
-    photos: PropTypes.array.isRequired
+    photos: PropTypes.array.isRequired,
+    setYear: PropTypes.func.isRequired
 }
--- src/reducers/page.js
+++ src/reducers/page.js
@@ -1,9 +1,16 @@
 const initialState = {
-    year: 2018,
+    year: 2271,
     photos: []
 }
 
-export default function page(state = initialState) {
-    return state
+export default function page(state = initialState, action) {
+    switch (action.type) {
+        case 'SET_YEAR':
+            return {...state, year: action.payload}
+
+        default:
+            return state
+    }
+
 }
 
